ec4df587d9480102002f67bd408311b2fd8169b5
Anderson Sousa <and3rsonsousa@outlook.com>
2022-03-01 19:54:24 -0300

    cores

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,7 +5,10 @@ export const loader: LoaderFunction = async ({ context }) => {
 	let db = createClient(context.SUPABASE_URL, context.SUPABASE_KEY, {
 		fetch: fetch.bind(self),
 	});
-	let data = await db.from("actions").select("*");
+	let { data } = await db
+		.from("actions")
+		.select("*, accounts(*), flows(*), tags(*), steps(*)")
+		.match({ user_id: "f5193773-261d-4984-aad1-39ea10e1d4ee" });
 
 	return { data };
 };
@@ -14,7 +17,7 @@ export default function Index() {
 	let data = useLoaderData();
 	return (
 		<div className="bg-gray-100 p-16">
-			<pre className="mb-8 rounded-md bg-gray-800 p-8 text-blue-500">
+			<pre className="text-blue-500 mb-8 rounded-md bg-gray-800 p-8">
 				{JSON.stringify(data, null, 2)}
 			</pre>
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,78 @@
 let colors = require("tailwindcss/colors");
 
 module.exports = {
-	content: ["./app/**/*.{js,jsx,ts,tsx}", "./styles/*.html"],
+	content: ["./app/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		colors: {
-			...colors,
-			gray: colors.slate,
-			brand: colors.indigo,
+			transparent: "transparent",
+			current: "currentColor",
+			white: "#ffffff",
+			black: "#000000",
+			gray: {
+				50: "#F4F5F7",
+				100: "#D8DBE2",
+				200: "#BCC1CC",
+				300: "#A1A7B5",
+				400: "#868E9E",
+				500: "#6C7486",
+				600: "#525B6E",
+				700: "#394255",
+				800: "#222C3A",
+				900: "#0F151C",
+			},
+			brand: {
+				50: "#F4F2FF",
+				100: "#D5CAFF",
+				200: "#BBA2FF",
+				300: "#A679FF",
+				400: "#9651FF",
+				500: "#8928FF",
+				600: "#8001FF",
+				700: "#6006CF",
+				800: "#4409A2",
+				900: "#2E0A76",
+			},
+			eletric: "#fa3889",
+			brave: "#ff6e4e",
+			bright: "#ffcc33",
+			neon: "#6ef76e",
+			pacific: "#0ce9da",
+			azure: "#3377ff",
+
+			error: colors.red,
+			success: colors.emerald,
+			warning: colors.yellow,
+
+			account: colors.violet,
+			planning: colors.blue,
+			copy: colors.teal,
+			creative: colors.green,
+			financial: colors.lime,
+
+			idea: colors.yellow,
+			do: colors.orange,
+			doing: colors.red,
+			review: colors.pink,
+			done: colors.purple,
+			accomplished: colors.indigo,
+
+			post: colors.purple,
+			stories: colors.rose,
+			reels: colors.amber,
+			meeting: colors.yellow,
+			copy: colors.emerald,
+			video: colors.sky,
+			shooting: colors.blue,
+			press: colors.indigo,
+			task: colors.lime,
+			tiktok: colors.pink,
 		},
-		extend: {},
 	},
+	extend: {
+		screens: {
+			"3xl": "2000px",
+		},
+	},
+
 	plugins: [require("@tailwindcss/forms")],
 };

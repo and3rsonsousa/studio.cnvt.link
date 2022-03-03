f147c49aa5b041ba82db734ee5752e963f3c648c
Anderson Sousa <and3rsonsousa@outlook.com>
2022-03-03 11:30:06 -0300

    patch

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -3,7 +3,10 @@ import type { Session } from "@supabase/supabase-js";
 
 export const supabase = createClient(
 	"https://prmvfibheijucdazdfzc.supabase.co",
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzY4MzI1MCwiZXhwIjoxOTU5MjU5MjUwfQ.ElgtQJthx_B3DM_zIL2acASAo_J_F9HclpLDv1m_hQ0"
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzY4MzI1MCwiZXhwIjoxOTU5MjU5MjUwfQ.ElgtQJthx_B3DM_zIL2acASAo_J_F9HclpLDv1m_hQ0",
+	{
+		fetch: fetch.bind(self),
+	}
 );
 
 export { Session };
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -5,15 +5,24 @@ import {
 	redirect,
 	useActionData,
 } from "remix";
-import { supabase } from "~/lib/supabase";
+// import { supabase } from "~/lib/supabase";
 import { commitSession, getSession } from "~/lib/session.server";
 import { HiOutlineLockClosed } from "react-icons/hi";
 import { useState } from "react";
+import { createClient } from "@supabase/supabase-js";
 
 export const action: ActionFunction = async ({ request }) => {
 	const formData = await request.formData();
 	let email = formData.get("email") as string;
 	let password = formData.get("password") as string;
+
+	const supabase = createClient(
+		"https://prmvfibheijucdazdfzc.supabase.co",
+		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzY4MzI1MCwiZXhwIjoxOTU5MjU5MjUwfQ.ElgtQJthx_B3DM_zIL2acASAo_J_F9HclpLDv1m_hQ0",
+		{
+			fetch: (...args) => fetch(...args),
+		}
+	);
 
 	let session = await getSession();
 	let auth = await supabase.auth.signIn({ email, password });
